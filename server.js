43e759de41277c0eeb7d7515f1ae3d0709a75062
HuseyinAltnsk <huseyinaltnskcsc@gmail.com>
2018-05-31 09:52:11 -0400

    Bootcamp-3 Huseyin-Eleni

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const mongoose = require("mongoose");
+const env = require('dotenv').config()
 // Initialize express
+const url = process.env.DB_DATABASE;
+mongoose.connect(url);
+require("./models/");
 const app = express();
 // Setup console logging
 app.use(morgan("tiny"));
 
 // TODO Initialize Mongoose.
+
+
 // TODO Define a schema for Todo.
 
+app.use(require("./routes"));
 // TODO Write your routes here. Interact with database as necessary.
-app.get("/", (req, res) => {
-  res.send({ message: "I'm alive!" });
-});
 
 // Listen to port 8080
-app.listen("8080");
+app.listen(process.env.SERVER_PORT);
